74a60918a0d7f15c805cb6c39e2e0b975bb46b4f
Pierre Manceron <pierre.manceron@gmail.com>
2017-09-06 16:52:54 +0200

    Rename to gym-extra

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='gymextra', version='0.0.1', install_requires=['gym'], packages=find_packages())
+setup(name='gym_extra', version='0.0.1', install_requires=['gym'], packages=find_packages())
